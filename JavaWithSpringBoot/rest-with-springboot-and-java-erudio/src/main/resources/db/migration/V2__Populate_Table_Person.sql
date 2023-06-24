1ed04c1d5d8cf4cfbbd485c9a49f58e84b88e10d
Weber Alvarenga <weber.alvarenga@gmail.com>
2023-06-24 15:26:29 -0300

    11 - Migratons com Flyway.

---
+++ JavaWithSpringBoot/rest-with-springboot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` VALUES 
+	(1,'Alameda 2517','Manoel','Masculino','Genoíno'),
+	(2,'Rua 325','Leonardo','Masculino','DaVinci'),
+	(4,'Detroit','Alex','Masculino','Murphy'),
+	(6,'Japão','Speed','Masculino','Racer'),
+	(7,'África do Sul','Nelson','Masculino','Mandela'),
+	(8,'Croácia','Nikola','Masculino','Tesla');
