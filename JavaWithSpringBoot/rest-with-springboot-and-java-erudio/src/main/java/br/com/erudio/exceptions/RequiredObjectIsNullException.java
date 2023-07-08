98cb59c8807c229686a038f02825f2d06f947b2b
Weber Alvarenga <weber.alvarenga@gmail.com>
2023-07-08 18:01:39 -0300

    14.2 - Testando com Mockito e JUnit5.

---
+++ JavaWithSpringBoot/rest-with-springboot-and-java-erudio/src/main/java/br/com/erudio/exceptions/RequiredObjectIsNullException.java
@@ -0,0 +1,20 @@
+package br.com.erudio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class RequiredObjectIsNullException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RequiredObjectIsNullException() {
+		super("Não é possível salvar um objeto nulo.");
+	}
+
+	public RequiredObjectIsNullException(String message) {
+		super(message);
+	}
+
+	
+}
